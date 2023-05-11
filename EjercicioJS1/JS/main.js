96aab070374ec8d9fccea0b92fdf778b6a8a1137
unknown <APRENDIZ@BOGDFPCPZ133.FormacionSena.RED>
2023-05-10 21:13:17 -0500

    Foolish exercises

---
+++ EjercicioJS1/JS/main.js
@@ -0,0 +1,16 @@
+function saludar() {
+    alert("Bienvenido Usuario :)");
+    alert("Acontinuacion encontras algunos botones que ejecutan algoritmos hechos con js puro");
+}
+
+function suma() {
+    let a = 0;
+    let b = 0;
+    let suma = 0;
+
+    a = parseInt(prompt("Ingrese el primer valor a sumar: "));
+    b = parseInt(prompt("Ingrese el segundo dato a sumar: "));
+    suma = a + b
+
+   alert("El resultado de la suma es: "+(b + a));
+}
